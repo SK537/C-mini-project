159cdf4159cc4216ad9f51983ccd865eb31b4397
SK537 <sabalkc462@gmail.com>
2017-03-11 23:54:52 +0545

    Update
    ...

--- game-7.cpp
+++ game-7.cpp
@@ -1,21 +1,35 @@
-#include<graphics.h> //comment
+#include<graphics.h>
 #include<stdio.h>
 #include<time.h>
 #include<conio.h>
 #include<dos.h>
 #include<windows.h>
+#include<string.h>
 int amount_won,life1,life2,life3,life4,file_num;
 char ans;
+char uname[50]="Guest account", pw[50];
+int successful_login=0;
 void sgame();
 int game(char*,char*,char*,char*,char*,char);
 void correct(int,int,int,int);
 void wrong(int,int,int,int);
 void window(void);
 void redgreen(int,int,int);
+void cheque(void);
+int user_login(void);
+void highscore(void);
+void user_score(void);
 
 void reset(void)                                        ///Resets amount won and lifelines left to zero
 {
     amount_won=0;life1=0;life2=0;life3=0;life4=0;
+}
+void gotoxy(int x, int y)                               ///Function to use gotoxy in codeblocks
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
 
@@ -25,48 +39,42 @@ int main()
     time_t current_time;
     current_time=time(NULL);
     system("cls");
-    printf("********************WELCOME TO KBC GAME********************");
+    printf("********************WELCOME TO KBC GAME********************   ");
     puts(ctime(&current_time));
+    if (successful_login==0)
+        printf("\n You are currently playing as a guest.To save your scores please login...");
     printf("\n\n \t \t \t 1.Start Game");
     printf("\n\n \t \t \t 2.High Score");
-    printf("\n\n \t \t \t 3.Help");
-    printf("\n\n \t \t \t 4.About");
-    printf("\n\n \t \t \t 5.Quit");
+    printf("\n\n \t \t \t 3.Login to user account");
+    printf("\n\n \t \t \t 4.Help");
+    printf("\n\n \t \t \t 5.About");
+    printf("\n\n \t \t \t 6.Quit");
     printf("\n\n \t \t \t SELECT YOUR OPTION(1/2/3/4/5): ");
     inputer=_getch();
     switch(inputer)
     {
         case '1':
-             printf("Starting the Game");
-             reset();
-             sgame();
-             break;
+            printf("Starting the Game");
+            reset();
+            sgame();
+            break;
         case '2':
             system("cls");
-
-            FILE *pf;
-            int high_score;
-            pf=fopen("highscore.txt","r");
-            fscanf(pf,"%d",&high_score);
-            printf("Your high score is %d",high_score);
-            printf("\nYour last score is %d",amount_won);
-            fclose(pf);
-            printf("\nPress r to reset highscore ");
-            printf("\n Press 2 to quit \n Press 1 to return to mainmenu");
-            a=_getch();
-            if(a=='1')
-            main();
-            else if(a=='2')
-            exit(0);
-            else if(a=='r')
+            if (successful_login==0)
             {
-                 pf=fopen("highscore.txt","w");
-                 fprintf(pf,"0");
-                 fclose(pf);
-                 main();
+                printf("You need to login to your user account to view user scores and highscores");
+                _getch();
+                main();
             }
+            else
+                highscore();
           break;
         case '3':
+            successful_login=user_login();
+            main();
+
+
+        case '4':
             system("cls");
             printf("\t \t \tHELP\t \t \t \n");
             printf("This game is based on one reality show.In this game there is 15 questions and 4 options in each questions with 4 lifeline.If your answer is correct you will win some money(Rs 5000 for 1st question and will doubled in each next questions)   and  proceed to next question.Lifelines are:-\n1.Phone a friend:You can call your friend and ask question to him/her.\n2.*2:-You can answer twice even your answer is wrong.\n3.Flip the question:-The question will removed and replaced by new one.\n(Note:You can't use same lifeline twice) ");
@@ -77,7 +85,7 @@ int main()
             else if(a=='2')
                 exit(0);
                 break;
-        case '4':
+        case '5':
             system("cls");
             printf("\t \t ABOUT GAME \t \t \n");
             printf("This game is developed by Group of Sabal,Rabindra,Subash and Shrawan(Students of Pulchowk campus,Computer Engineering) ");
@@ -88,7 +96,7 @@ int main()
             else if(a=='2')
                 exit(0);
                 break;
-        case '5':
+        case '6':
             exit(0);
 
         default:
@@ -112,23 +120,23 @@ void sgame()
     if (fp==NULL)
         printf("File not read successfully...");
 
-    int flag,len,line;
+    int flag,len,file_line;
     char  line1[100];
     ///Read a random line/question from file:
     while (1)
     {
-        line=rand()%6;
-        for (i=0;i<index;i++)               //To prevent repetition of questions
+        file_line=rand()%10;
+        for (i=0;i<index;i++)                       ///To prevent repetition of questions
         {
-            if (linenumber[i]==line)
+            if (linenumber[i]==file_line)
             {
-                    line=rand()%6;
+                    file_line=rand()%6;
                     i=0;
             }
 
 
         }
-        linenumber[index]=line;
+        linenumber[index]=file_line;
         index++;
         flag=0;
         while(!feof(fp))
@@ -137,7 +145,7 @@ void sgame()
         len = strlen(line1);
         if(line1[len-1] == '\n')
             {
-            if (flag==line)
+            if (flag==file_line)
                 {
 
                     fscanf(fp,"%[^!]",&ques);
@@ -150,9 +158,13 @@ void sgame()
 
         }
         status=game(ques,a,b,c,d,correct);
+        fflush(stdin);
         if (status==0)
         {
+            ///*******highscores**********
             FILE *pf;
+            time_t mytime;
+            mytime = time(NULL);
             pf=fopen("highscore.txt","r");
             while(!feof(pf))
             {
@@ -161,18 +173,36 @@ void sgame()
                 if(amount_won>file_num)
                 {
                     pf=fopen("highscore.txt","w");
-                    fprintf(pf,"%d",amount_won);
+                    fprintf(pf,"Player name: %s\nPlayer highscore: %d\nPlayed date:%s",uname,amount_won,ctime(&mytime));
                     fclose(pf);
                     break;
                 }
                 break;
             }
+            ///*******user records**********
+            FILE *info;
+            info=fopen("record.txt","a+");
+            if (successful_login==1)
+            {
+                fprintf(info,"Player Name :%s\n",uname);
+                fprintf(info,"Played Date:%s",ctime(&mytime));
+                fprintf(info,"Score:%d\n",amount_won);
+                int k;
+                for(k=0; k<=50; k++)
+                    fprintf(info,"%c",'_');
+                fprintf(info,"\n");
+                fclose(info);
+            }
+
             break;
         }
         rewind(fp);
+
     }
     fclose(fp);
     closegraph();
+    cheque();
+
     main();
 }
 
@@ -181,7 +211,6 @@ void sgame()
 void window()
 {
     int gd=DETECT,gm;
-    //initgraph(&gd,&gm,"C:\\TURBOC3\\bgi");
     initwindow(1360,700,"Sample for mini project");
     setfillstyle(SOLID_FILL,BLUE);
     floodfill(1,1,WHITE);
@@ -439,62 +468,62 @@ while(1)
         return 0;
 
 }
-
 void correct(int a,int b,int c,int d)
 {
-
     int i;
     if(amount_won==0)
-        amount_won=amount_won+500;
-    else
-        amount_won=amount_won*2;
-    rectangle(a,b,c,d);
-    setfillstyle(HATCH_FILL,YELLOW);
-    floodfill(a+1,b+1,WHITE);
-    delay(2000);
-    for (i=0;i<5;i++)
-    {
-        if (i%2==0)
+    amount_won=amount_won+500;
+    else amount_won=amount_won*2;
+
+
+        rectangle(a,b,c,d);
+        setfillstyle(SOLID_FILL,YELLOW);
+        floodfill(a+1,b+1,WHITE);
+        delay(2000);
+        for (i=0;i<5;i++)
         {
-            setfillstyle(HATCH_FILL,GREEN);
-            rectangle(a,b,c,d);
-            floodfill(a+1,b+1,WHITE);
-            delay(300);
-        }
-        else
-        {
-            setfillstyle(HATCH_FILL,BLUE);
-            rectangle(a,b,c,d);
-            floodfill(a+1,b+1,WHITE);
-            delay(300);
-        }
-    }
+            if (i%2==0)
+            {
+                setfillstyle(SOLID_FILL,GREEN);
+                rectangle(a,b,c,d);
+                floodfill(a+1,b+1,WHITE);
+                delay(300);
+            }
+            else
+            {
+                setfillstyle(XHATCH_FILL,BLUE);
+                rectangle(a,b,c,d);
+                floodfill(a+1,b+1,WHITE);
+                delay(300);
+            }
+         }
+
 }
 
 void wrong(int a,int b,int c,int d)
 {
-    rectangle(a,b,c,d);
-    int i;
-    setfillstyle(HATCH_FILL,YELLOW);
-    floodfill(a+1,b+1,WHITE);
-    delay(2000);
-    for (i=0;i<5;i++)
-    {
-        if (i%2==0)
+        rectangle(a,b,c,d);
+        int i;
+        setfillstyle(SOLID_FILL,YELLOW);
+        floodfill(a+1,b+1,WHITE);
+        delay(2000);
+        for (i=0;i<5;i++)
         {
-            setfillstyle(HATCH_FILL,RED);
-            rectangle(a,b,c,d);
-            floodfill(a+1,b+1,WHITE);
-            delay(300);
-        }
-        else
-        {
-            setfillstyle(HATCH_FILL,BLUE);
-            rectangle(a,b,c,d);
-            floodfill(a+1,b+1,WHITE);
-            delay(300);
-        }
-    }
+            if (i%2==0)
+            {
+                setfillstyle(SOLID_FILL,RED);
+                rectangle(a,b,c,d);
+                floodfill(a+1,b+1,WHITE);
+                delay(300);
+            }
+            else
+            {
+                setfillstyle(XHATCH_FILL,BLUE);
+                rectangle(a,b,c,d);
+                floodfill(a+1,b+1,WHITE);
+                delay(300);
+            }
+         }
 }
 
 void redgreen(int x,int y, int greenred)
@@ -523,3 +552,224 @@ void redgreen(int x,int y, int greenred)
 
 }
 
+void cheque(void)
+{
+    initwindow(1010,700,"CHEQUE");
+    setfillstyle(HATCH_FILL,BLUE);
+    floodfill(100,100,WHITE);
+    bar(0,0,1010,700);
+    floodfill(100,100,WHITE);
+    setcolor(YELLOW);
+     for(int i=10;i>=0;i--)
+    {
+            rectangle(10-i,100-i,1000-i,600-i);
+
+    }
+    settextstyle(1,HORIZ_DIR,3);
+    setcolor(YELLOW);
+    char damount[100];
+    sprintf(damount,"CONGRATULATIONS YOU HAVE WON Rs.%d",amount_won);
+    outtextxy(1,1,damount);
+    setfillstyle(WIDE_DOT_FILL,RED);
+    floodfill(140,250,YELLOW);
+    setcolor(WHITE);
+    settextstyle(1,HORIZ_DIR,5);
+    outtextxy(20,110,"KBC WINNER'S CHEQUE");
+    //setcolor(WHITE);
+    settextstyle(1,HORIZ_DIR,2);
+    outtextxy(20,220,"NAME:");
+    char ch2[20];
+    sprintf(ch2,"Rs.%d",amount_won);
+    outtextxy(160,210,uname);
+    rectangle(150,200,600,250);
+    outtextxy(20,320,"AMOUNT WON:");
+    settextstyle(1,HORIZ_DIR,4);
+    setcolor(YELLOW);
+    outtextxy(350,310,ch2);
+    setcolor(WHITE);
+    rectangle(290,300,750,350);
+     settextstyle(1,HORIZ_DIR,2);
+    outtextxy(20,420,"ACCOUNT NO:");
+    long int accountno=1222056745;
+    char acc[100];
+    int t=textwidth("ACCOUNT NO:");
+    sprintf(acc,"%ld",accountno);
+    settextstyle(1,HORIZ_DIR,4);
+     rectangle(270,400,720,450);
+    setcolor(YELLOW);
+    outtextxy(t+70,410,acc);
+
+    setcolor(YELLOW);
+    settextstyle(4,HORIZ_DIR,2);
+    outtextxy(820,560,"SIGNATURE");
+    setlinestyle(1,1,3);
+        line(820,550,990,550);
+    settextstyle(5,HORIZ_DIR,4);
+    setcolor(WHITE);
+    outtextxy(820,510,"Rabindra");
+
+    //delay(8000);
+    getch();
+    closegraph();
+}
+
+int user_login(void)
+{
+    FILE *fp, *log;
+    char  usr[50], rpw[50],pp[50];
+    int found=0;
+    char opt;
+    system("cls");
+    printf("\n\n\n\n\t\t1.Add Acount\n\n\t\t2.Login\n\n\t\t3.Exit to main menu\n\n");
+    printf("\t\tYour option is: ");
+    opt=getche();
+
+    switch(opt)
+    {
+    case '1':
+        system("cls");
+        printf("Create your account here: ");
+        printf("\n\n\n\n\t\tEnter any username: \n\n");
+        printf("\t\tEnter your password: \n\n");
+        printf("\t\tRe-enter your password: \n\n");
+        gotoxy(36,4);
+        gets(uname);
+        gotoxy(37,6);
+        gets(pw);
+        gotoxy(40,8);
+        gets(rpw);
+        while(_kbhit())
+            printf("\r*");
+        printf("\n\t\tSelect: 1.OK\n\t\t\t2.Go back");
+        opt=_getch();
+        if(opt=='1')
+        {
+            if(strcmp(pw,rpw)==0)
+            {
+                fp=fopen("abc.txt","a+");
+                if(fp==NULL)
+                    printf("File not found!!");
+                fprintf(fp,"%s %s\n",uname,pw);
+                fclose(fp);
+                system("cls");
+                printf("Welcome to Game!!");
+            }
+            else
+               {
+
+                printf("\n\tPassword didn't match!!\n\tTry to create acoount again!!\n");
+                Sleep(3000);
+                system("cls");
+                user_login();
+               }
+        }
+        else if(opt=='2')
+        {
+            system("cls");
+            user_login();
+        }
+        else
+            printf("Choose correct option!!");
+        break;
+    case '2':
+        system("cls");
+        printf("Login to your account here: ");
+        printf("\n\n\n\n\t\tEnter your username: \n\n");
+        printf("\t\tEnter your password: \n\n");
+
+        gotoxy(37,4);
+        gets(usr);
+
+        gotoxy(37,6);
+        gets(pp);
+        printf("\n\t\tSelect: 1.OK\n\t\t\t2.Go back");
+        opt=_getch();
+        if(opt=='1')
+        {
+            log=fopen("abc.txt","r");
+            if(log==NULL)
+                printf("File not found!!");
+           while(!feof(log))
+            {
+                fscanf(log,"%s %s",uname,pw);
+                if(strcmp(usr,uname) == 0 && strcmp(pp,pw)==0)
+                 {
+                    found = 1;
+                    break;
+                 }
+            }
+            if(found == 1)
+            {
+                system("cls");
+                printf("Welcome!");
+                _getch();
+                return 1;
+            }
+            else
+                printf("\nInvalid username or password");
+            fclose(log);
+        }
+
+    else if(opt=='2')
+    {
+        system("cls");
+        user_login();
+    }
+    else
+        printf("Enter valid key");
+    break;
+    case '3':
+        main();
+
+    default:
+        printf("\n\t\tEnter valid key");
+        delay(1000);
+        user_login();
+    }
+    _getch();
+}
+
+
+void highscore()
+{
+    system("cls");
+    FILE *pf;
+    int high_score;
+    char a,c;
+    pf=fopen("highscore.txt","r");
+        do
+        {
+            putchar(c=getc(pf));
+        }
+        while(c!=EOF);
+    fclose(pf);
+    printf("\nPress r to reset highscore\n ");
+    printf("\nPress 1 to return to mainmenu\n Press 2 to view user scores");
+    a=_getch();
+    if(a=='1')
+        main();
+    else if(a=='2')
+        user_score();
+    else if(a=='r')
+    {
+        pf=fopen("highscore.txt","w");
+        fprintf(pf,"0");
+        fclose(pf);
+        main();
+    }
+}
+
+void user_score(void)
+{
+        system("cls");
+        FILE *user;
+        char c;
+        user=fopen("record.txt","r");
+        do
+        {
+            putchar(c=getc(user));
+        }
+        while(c!=EOF);
+        _getch();
+        main();
+}
